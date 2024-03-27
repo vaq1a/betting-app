4cf7744fda5848c37d462f36c51aba866aaf9b03
vaq1a <vadimis26@gmail.com>
2024-03-27 18:59:18 +0300

    feat: layout

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -1,6 +1,7 @@
 const swiper = new Swiper('.banners-slider', {
     speed: 400,
     spaceBetween: 10,
+    slidesPerView: 1,
     navigation: {
         nextEl: '.banners-slider__button--next',
         prevEl: '.banners-slider__button--prev',
